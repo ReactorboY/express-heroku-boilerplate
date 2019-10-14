2d8771dc5f69d7d75949498a5c6a1bc565c79101
Mohd Hussain <samsteve124@gmail.com>
2019-10-14 09:48:10 +0530

    theme changed now portfolio

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {BrowserRouter as Router,Route} from 'react-router-dom'
+import Home from './Components/Home/Home'
 
 // Pages Component
 
 function App() {
   return (
         <Router>
-          
+          <Route exact to="/" component={Home} />
         </Router>
   );
 }
