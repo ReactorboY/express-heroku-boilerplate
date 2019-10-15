731a574d83915ea4ea497e75e4c37311e4b2aa1b
Mohd Hussain <samsteve124@gmail.com>
2019-10-15 08:57:01 +0530

    new modifications

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {BrowserRouter as Router,Route} from 'react-router-dom'
 import Home from './Components/Home/Home'
 
 // Pages Component
 
-function App() {
+class App extends Component {
+  render(){
   return (
         <Router>
+          
           <Route exact to="/" component={Home} />
         </Router>
-  );
+    )
+  }
 }
 
 export default App;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import Nav from '../Common/Nav'
+import Header from '../Common/Header'
 
 class Home extends Component {
     render() {
         return (
             <div>
-                
+                <Nav />
+                <Header />
             </div>
         )
     }
